04d5da911f9a2560600857a9ee93842b39df65dd
SoA-Lee <llsa0927@naver.com>
2022-12-08 22:23:43 +0900

    Feat - Transaction Lock 처리

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,74 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void insert() {
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void delete() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("재고 감소 테스트")
+    public void decrease_test() {
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - 1 = 99
+
+        assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    @DisplayName("동시에 100개 요청 테스트 - Race Condition 발생")
+    public void decreaseTestFailedByRaceCondition() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (100 * 1) = 0
+        assertEquals(0, stock.getQuantity());
+    }
+}
